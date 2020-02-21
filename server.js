23db41378b564a310ddfb8463d199b5b2f2dfc54
Andrea Harris <Andrea@zacs-imac.lan>
2020-02-21 11:11:19 -0600

    server/index

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const ProjectRouter =require("./projects/projects-router");
+const server = express();
+
+server.use(express.json());
+server.use("/api/project", ProjectRouter);
+
+module.exports = server;
