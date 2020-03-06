74a3c71393ee3f03656d4397ecce60003ea21cff
Andrea Harris <Andrea@zacs-imac.lan>
2020-03-06 11:18:47 -0600

    finished

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const taskRouter = require("./tasks/taskRouter");
 const server= express();
 
 server.use(express.json())
-server.use("api/projects", projectsRouter)
-server.use("api/resources", resourcesRouter)
-server.use("api/tasks", taskRouter)
+server.use("/api/projects", projectsRouter)
+server.use("/api/resources", resourcesRouter)
+server.use("/api/tasks", taskRouter)
+
+server.get('/', (req, res) => {
+    res.send("Look at this!");
+  });
 
 const port = process.env.PORT || 5000;
 
